0054df9c696960f6432bc42c5e8c5b775b780fbf
kanishma99 <kanishma.kani99@gmail.com>
2025-05-02 15:18:23 +0530

    Create a New class

---
+++ src/main/java/com/bbi/Bbi.java
@@ -0,0 +1,19 @@
+package com.bbi;
+
+public class Bbi {
+	private void onboatding() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void handbook() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void onsite() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/bbi/Office.java
@@ -0,0 +1,17 @@
+package com.bbi;
+
+public class Office {
+	private void gjk() {
+		// TODO Auto-generated method stub
+
+	}
+	private void nll() {
+		// TODO Auto-generated method stub
+
+	}
+	private void vdb() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
